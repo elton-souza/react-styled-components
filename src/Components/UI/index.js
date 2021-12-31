f9dcc3c5d6ef4ece5b520b165283a21323bbe758
elton-souza <elton.souza1515@gmail.com>
2021-12-31 09:57:51 -0300

    adicioando styled components

---
+++ src/Components/UI/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Icone = styled.img`
+  height: 25px;
+  width: 25px;
+`;
+
+export default Icone
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,5 @@
+const corPrimaria = "#41d3be";
+const fundoClaro = "#f1f1f1";
+const textoFundoClaro = "grey";
+
+export {corPrimaria, fundoClaro, textoFundoClaro}
